c0e7b021ab30a395d30ae771d16168a651a9f149
Alessandro Caproni <acaproni@eso.org>
2025-10-09 14:56:59 +0200

    Reads the json configuration

--- FilePlugin/src/main/bin/filePlugin.sh
+++ FilePlugin/src/main/bin/filePlugin.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp "build/libs/*:$HOME/IasRoot/lib/*:$HOME/IasRoot/lib/ExtLibs/*" org.eso.ias.tutorial.fileplugin.FilePlugin
+java -cp "build/libs/*:$HOME/IasRoot/lib/*:$HOME/IasRoot/lib/ExtLibs/*" org.eso.ias.tutorial.fileplugin.FilePlugin $@
 
--- FilePlugin/src/main/java/org/eso/ias/tutorial/fileplugin/FilePlugin.java
+++ FilePlugin/src/main/java/org/eso/ias/tutorial/fileplugin/FilePlugin.java
@@ -1,9 +1,14 @@
 package org.eso.ias.tutorial.fileplugin;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.eso.ias.plugin.Plugin;
+import org.eso.ias.plugin.config.PluginConfigException;
 import org.eso.ias.plugin.config.PluginFileConfig;
+import org.eso.ias.plugin.config.PluginConfigFileReader;
 
 public class FilePlugin extends Plugin {
 
@@ -11,15 +16,27 @@ public class FilePlugin extends Plugin {
    * Constructor
    */
   private FilePlugin(PluginFileConfig config) {
-    
-    
     super(config);
   }
 
   /**
    * runs the plugin.
    */
-  public static void main(String[] args) throws IOException {
-    System.err.println("Starting FilePlugin...");
+  public static void main(String[] args) throws Exception {
+    if (args.length!=1) {
+      System.err.println("Wrong number of params given: config file missing!");  
+      System.exit(2);
+    }
+    System.out.println("Starting FilePlugin with config file "+args[0]);
+
+    try {
+      File file = new File(args[0]);
+      PluginConfigFileReader jsonFileReader = new PluginConfigFileReader(file);
+      PluginFileConfig config = jsonFileReader.getPluginConfig().get(1,TimeUnit.MINUTES);
+    } catch (Throwable t) {
+      System.out.println(t.getMessage());
+      t.printStackTrace(System.err);
+      System.exit(1);
+    }
   }
 }
